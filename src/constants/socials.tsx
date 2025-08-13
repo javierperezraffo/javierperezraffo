94ee200a8838038fa3092134ee2d0aa0272b92bd
javierperezraffo <javierperezraffo@gmail.com>
2025-08-13 17:10:10 -0300

    Force Vercel redeploy: Added blank space to socials.tsx

--- src/constants/socials.tsx
+++ src/constants/socials.tsx
@@ -1,23 +1,12 @@
 import {
   IconBrandLinkedin,
-  IconBrandTwitter,
-  IconBrandYoutube,
 } from "@tabler/icons-react";
 
 export const socials = [
   {
-    href: "https://twitter.com/mannupaaji",
-    label: "Twitter",
-    icon: IconBrandTwitter,
-  },
-  {
-    href: "https://linkedin.com/in/manuarora28",
+    href: "https://www.linkedin.com/in/javierperezraffo/",
     label: "LinkedIn",
     icon: IconBrandLinkedin,
   },
-  {
-    href: "https://youtube.com/maninthere",
-    label: "YouTube",
-    icon: IconBrandYoutube,
-  },
 ];
+
