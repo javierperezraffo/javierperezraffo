688297e94bb92f449243de95e39108f66820f20d
javierperezraffo <javierperezraffo@gmail.com>
2025-08-15 19:33:12 -0300

    fix error container

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-export const Container = ({ children }: { children: React.ReactNode }) => {
+export const Container = ({ children, className }: { children: React.ReactNode; className?: string }) => {
   return (
-    <main className={`max-w-4xl w-full mx-auto py-20 px-4 md:px-10`}>
+    <main className={`max-w-4xl w-full mx-auto py-20 px-4 md:px-10 ${className}`}>
       {children}
     </main>
   );
